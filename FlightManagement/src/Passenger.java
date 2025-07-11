2718094b5f63616a822c3d54ff998e5513bf23eb
mmammadli16245 <129931893+MaryamMammadli@users.noreply.github.com>
2025-07-11 13:36:32 +0400

    Update Passenger.java

--- FlightManagement/src/Passenger.java
+++ FlightManagement/src/Passenger.java
@@ -1,4 +1,6 @@
-public class Passenger {
+import java.io.Serializable; //object can be saved to a file in binary format
+
+public class Passenger implements Serializable{
     
 
     private String name;
@@ -6,13 +8,61 @@ public class Passenger {
     private String passportID;
     private String seat;
     private String flightID;
-    private String passemgerID;
+    private String passengerID;
 
     // constructor
-    // getter and settes
-
-    public void changeSeat(String new_seat){
-        this.seat = new_seat;
+    public Passenger(String name, String surname, String passportID, String seat, String flightID, String passengerID) {
+        this.name = name;
+        this.surname = surname;
+        this.passportID = passportID;
+        this.seat = seat;
+        this.flightID = flightID;
+        this.passengerID = passengerID;
     }
+
+    // getter and settes
+    public String getName(){
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getPassportID() {
+        return passportID;
+    }
+    public String getSeat() {
+        return seat;
+    }
+    public String getFlightID() {
+        return flightID;
+    }
+    public String getPassengerID() {
+        return passengerID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setPassportID(String passportID) {
+        this.passportID = passportID;
+    }
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
+    public void setPassengerID(String passengerID) {
+        this.passengerID = passengerID;
+    }
+
+    public void changeSeat(String newSeat){
+        this.seat = newSeat;
+    }
+
+    
 
 }
