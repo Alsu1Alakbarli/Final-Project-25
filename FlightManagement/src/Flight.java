ae934f62a6cf88a057e04629e0d6ee2e1422a73e
mmammadli16245 <129931893+MaryamMammadli@users.noreply.github.com>
2025-07-11 16:59:56 +0400

    Update Flight.java

--- FlightManagement/src/Flight.java
+++ FlightManagement/src/Flight.java
@@ -1,15 +1,91 @@
-class Flight{
+import java.io.Serializable;
+import java.util.ArrayList;
+
+class Flight implements Serializable{
 
     private String departure;
     private String arrive;
     private String departure_time;
     private String arrival_time;
     private int price;
+    private String flightID;
+    private String airlineName;
+    private int seatCapacity;
+
+    private ArrayList<Passenger> passengers = new ArrayList<>(); //List of passengers
+    
     // cosntructor
+    public Flight(String departure, String arrive, String departure_time, String arrival_time, String flightID, String airlineName, int price, int seatCapacity) {
+        this.departure = departure;
+        this.arrive = arrive;
+        this.departure_time = departure_time;
+        this.arrival_time = arrival_time;
+        this.flightID = flightID;
+        this.airlineName = airlineName;
+        this.price = price;
+        this.seatCapacity = seatCapacity;
+    }
     // getter and setters
+    public String getDeparture() {
+        return departure;
+    }
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+    public String getArrive() {
+        return arrive;
+    }
+    public void setArrive(String arrive) {
+        this.arrive = arrive;
+    }
+    public String getDeparture_time() {
+        return departure_time;
+    }
+    public void setDeparture_time(String departure_time) {
+        this.departure_time = departure_time;
+    }
+    public String getArrival_time() {
+        return arrival_time;
+    }
+    public void setArrival_time(String arrival_time) {
+        this.arrival_time = arrival_time;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getFlightID() {
+        return flightID;
+    }
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
+    public String getAirlineName() {
+        return airlineName;
+    }
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+    public int getSeatCapacity() {
+        return seatCapacity;
+    }
+    public void setSeatCapacity(int seatCapacity) {
+        this.seatCapacity = seatCapacity;
+    }
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+
 
     public void delay(int delay_in_minute){
         this.departure_time = this.departure_time + delay_in_minute;
         this.arrival_time = this.arrival_time + delay_in_minute;
     }
+
 }
