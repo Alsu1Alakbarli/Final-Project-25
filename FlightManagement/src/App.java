a827987b7b2f9a0461179045b664d8e80a77d185
nazrin16204 <nazringuliyevaaa5@gmail.com>
2025-07-14 02:00:58 +0400

    Added BookingManager and Menu System (CLI)

--- FlightManagement/src/App.java
+++ FlightManagement/src/App.java
@@ -1,17 +1,74 @@
+import java.util.Scanner;
+
 public class App {
+ 
     public static void main(String[] args) throws Exception {
-        // 5 dene flight obyekt 
+ 
+        Scanner scn = new Scanner(System.in);
+        BookingManager manager =  new BookingManager();
 
-        // her flight obyekti ucun 5-15 arasi passenger yarat
+        while (true) {
+            System.out.println("=========  Welcome to the Flight Management Menu  =========");
+            System.out.println("1. Create a new flight");
+            System.out.println("2. List all existed flights");
+            System.out.println("3. Search flights by any attribute");
+            System.out.println("4. Sort flights by price");
+            System.out.println("5. Sort flights by departure city");
+            System.out.println("6. Sort flights by departure time");
+            System.out.println("7. Edit a flight");
+            System.out.println("8. Delete a flight");
+
+            System.out.println("\n");
+            System.out.println("Please enter your choice: ");
+        
+            int user_in = scn.nextInt();
+            
+            switch (user_in) {
+                case 1:
+                    System.out.println("User wants to ccreate a new fligth:");
+                    
+                    System.out.print("Flight ID: ");
+                    String fligt_id = scn.nextLine();
+
+                    System.out.print("Departure city: ");
+                    String dept_city = scn.nextLine();
+
+                    System.out.print("Arrival city: ");
+                    String arr_city = scn.nextLine();
+
+                    System.out.print("Departure time: ");
+                    String dept_time = scn.nextLine();
+
+                    System.out.print("Arrival time: ");
+                    String arr_time = scn.nextLine();
+                    
+                    System.out.print("Price:  ");
+                    int price = scn.nextInt();
+
+                    System.out.print("Airlinename: ");
+                    String airline_name = scn.nextLine();
+
+                    System.out.print("Seat capacity: ");
+                    int seat_cap = scn.nextInt();
+                    
+                    
+                    Flight new_filght = new Flight(dept_city, arr_city, dept_time, arr_time, fligt_id, airline_name, price, seat_cap);
+                    manager.createFlight(new_filght);
+                    
+                    break;
 
 
-        // Provide all core functionalities: 
-        // List all flights (with total count on top)
-        // Search flights by any attribute
-        // Sort flights by any field (price, date, city, etc.)
-        // Edit/update after search
-        // Delete after search
-        // Error handling
+
+                
+
+                case 2:
+                    System.out.println("User wants to list all existed flights");
+                default:
+                    break;
+            }
+
+            break;
+        }
 
 
     }
--- FlightManagement/src/BookingManager.java
+++ FlightManagement/src/BookingManager.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class BookingManager {
 
@@ -76,8 +77,100 @@ public class BookingManager {
     }
 
     // departime city
+    public void sortFlightsByDepartureCity(){
+        flights.sort(Comparator.comparing(Flight::getDeparture));
+        System.out.println("Flights sorted by departure city");
+    }
 
     // departure time
-    
+    public void sortFlightsByDepartureTime(){
+        flights.sort(Comparator.comparing(Flight:: getDeparture_time));
+        System.out.println("Flights sorted by departure time");
+    }
+
+    public boolean editFlight(String flightID, Scanner sc){
+        Flight flight = findFlightByID(flightID);
+
+        System.out.println("Now you are editing/updating flight info:");
+        System.out.println("Which parameter do you want to edit from the following list: ");
+        System.out.println("\t\t[Departure, Arrive, Departure_time, Arrival_time, Price, Flight_ID, AirlineName, SeatCapacity]");
+        System.out.println("Please enter the attribte from the above list: ");
+
+        String user_in = sc.nextLine();
+        boolean flag = false;
+        switch (user_in) {
+            case "Departure":
+                System.out.println("Please enter new departure destination: ");
+                String new_departure = sc.nextLine();
+                flight.setDeparture(new_departure);
+                flag = true;
+                break;
+
+            case "Arrive":
+                System.out.println("Please enter new arrive destination: ");
+                String new_arrive = sc.nextLine();
+                flight.setArrive(new_arrive);
+                flag = true;
+                break;
+        
+            case "Departure_time":
+                System.out.println("Please enter new separture time: ");
+                String new_dept_time = sc.nextLine();
+                flight.setDeparture_time(new_dept_time);
+                flag = true;
+                break;
+
+                
+            case "Arrival_time":
+                System.out.println("Please enter new arrival time: ");
+                String new_arr_time = sc.nextLine();
+                flight.setArrival_time(new_arr_time);
+                flag = true;
+                break;
+            
+                
+            case "Price":
+                System.out.println("Please enter new price: ");
+                int new_price = sc.nextInt();
+                flight.setPrice(new_price);
+                flag=true;
+                break;
+
+             case "AirlineName":
+                System.out.println("Please enter new AirlineName: ");
+                String new_airline_name = sc.nextLine();
+                flight.setAirlineName(new_airline_name);
+                flag=true;
+                break;
+
+             case "SeatCapacity":
+                System.out.println("Please enter new SeatCapacity: ");
+                int new_seat_capacity = sc.nextInt();
+                flight.setSeatCapacity(new_seat_capacity);
+                flag = true;
+                break;
+
+            
+            
+            default:
+                System.out.println("Your input is not reliable");
+                break;
+        }
+
+        return flag;
+
+    }
+
+    public boolean deleteFlight(String flight_ID){
+
+        Flight flight = findFlightByID(flight_ID);
+
+        if (flight != null){
+            flights.remove(flight);
+            return true;
+        }
+
+        return false;
+    }
     
 }
